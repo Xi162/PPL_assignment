3be7de15d290f8f6902d51cae572970920656557
Xi162 <buihuy1602@gmail.com>
2024-03-11 12:07:42 +0700

    add testcase

--- assignment2/src/test/ASTGenSuite.py
+++ assignment2/src/test/ASTGenSuite.py
@@ -181,3 +181,15 @@ end
         expect = str(Program([FuncDecl(Id("main"), [], Block([
             If(BooleanLiteral(True), If(BinaryOp("and", BooleanLiteral(True), BooleanLiteral(True)), Return(NumberLiteral(1.0)), [(BooleanLiteral(True), Return(NumberLiteral(2.0)))], Return(NumberLiteral(3.0))))]))]))
         self.assertTrue(TestAST.test(input, expect, 315))
+
+    def test_simple_program_17(self):
+        input = """func main() begin
+        if (true) if (true and true) return 1
+        elif (true) return 2
+        else return 3
+        else return 4
+        end
+        """
+        expect = str(Program([FuncDecl(Id("main"), [], Block([
+            If(BooleanLiteral(True), If(BinaryOp("and", BooleanLiteral(True), BooleanLiteral(True)), Return(NumberLiteral(1.0)), [(BooleanLiteral(True), Return(NumberLiteral(2.0)))], Return(NumberLiteral(3.0))), [], Return(NumberLiteral(4.0)))]))]))
+        self.assertTrue(TestAST.test(input, expect, 316))
